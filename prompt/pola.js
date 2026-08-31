81aded256fb401138ea0bf22d599145d1ffb9bdc
Estarossa1233 <ferdi880808@gmail.com>
2026-08-31 21:02:17 +0700

    Add files via upload

---
+++ prompt/pola.js
@@ -0,0 +1,23 @@
+module.exports = `
+
+⚡️ - 𝐏𝐑𝐀𝐆𝐌𝐀𝐓𝐈𝐂 𝐏𝐋𝐀𝐘 -
+
+DC : ON
+Otomatis 30X ❌ ✅ ✅
+Manual 16X ❌ ✅ ✅
+
+DC : OFF
+Otomatis 30X ✅ ❌ ✅
+Manual 16X ✅ ❌ ❌
+
+BUY SPIN BET BEBAS
+
+––––––––––––––––––
+
+🎰 - 𝐏𝐆 𝐒𝐎𝐅𝐓 -
+
+Manual 17X TURBO : ✅
+Otomatis 30X TURBO : ❌
+Manual 22X TURBO : ❌
+Otomatis 30X TURBO : ✅
+`;
